1c9035ee9dac01cfd4ca04162c74796a4f62506a
Beniamin Cosma <cosma.beny@gmail.com>
2018-11-12 18:20:08 +0200

    add fps log message

--- src/player/flv-player.js
+++ src/player/flv-player.js
@@ -626,6 +626,7 @@ class FlvPlayer {
         const id = this._mediaElement.id;
         const baseFps = this._mediaDataSource.fps ? this._mediaDataSource.fps : 30;
         const fps = this._mediaInfo ? this._mediaInfo.fps : baseFps;
+        console.log('Using fps: ', fps);
         const video = new VideoFrame({
             id: id,
             frameRate: fps,
